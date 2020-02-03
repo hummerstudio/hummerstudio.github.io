bae0c154a43ae2c9d8994a83259b1a505738af3b
TangMing <hummerstudio@163.com>
2020-02-03 10:51:29 +0800

    注释gitment相关代码

--- static/js/post.js
+++ static/js/post.js
@@ -8,7 +8,7 @@
 $(document).ready(function() {
     generateContent();
     // share();
-    gitment();
+    // gitment();
 });
 
 /**
@@ -27,7 +27,7 @@ function share(){
     with(document)0[getElementsByTagName("script")[0].parentNode.appendChild(createElement('script')).src='//bdimg.share.baidu.com/static/api/js/share.js?v=89860593.js?cdnversion='+~(-new Date()/36e5)];
 }
 
-
+/**
 function gitment() {
     var gitment = new Gitment({
         id: window.location.pathname,
@@ -41,5 +41,5 @@ function gitment() {
     gitment.render('post-comment')
     $("#post-comment").removeClass('hidden');
 }
-
+*/
 
